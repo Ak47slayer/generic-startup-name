be856afe81a246453fbc13d37010d2e8009e527b
Akshay <coolakshay.pati95@gmail.com>
2016-09-02 04:41:17 +0530

    changes

--- manage.py
+++ manage.py
@@ -5,12 +5,9 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "startproject.settings")
 
-    import django.core.management.commands.runserver as runserver
-    runserver.DEFAULT_PORT="8197"
-    from django.core.management.commands.runserver import Command
+   
 
     from django.core.management import execute_from_command_line
 
 
     execute_from_command_line(sys.argv)
-    
